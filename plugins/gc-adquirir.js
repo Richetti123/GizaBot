1332bb90f48e3770aa2f15c6fe3cf8f0973cd202
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-07-28 22:50:01 +0200

    Update gc-adquirir.js

--- plugins/gc-adquirir.js
+++ plugins/gc-adquirir.js
@@ -144,17 +144,12 @@ nombreLugar = 'America'
 ciudad = 'Lima'
 }	
 let margen = '*··················································*'
-let menu = `HOLA VEO QUE ESTAS INTERESADO EN COMPRAR EL BOT
+let menu = `¿Estás interesado en comprar el bot?
 
-> 🤖Si quieres que te atienda nuestro asistente virtual comunícate a este número +56997524559🤖
+Contactame: +52 777 266 8134 ⚡
 
-Y si quieres que te atienda una persona real escríbele a nuestros creadores:
-Geri: +56 9 4182 7124
-Richetti: +56 9 2856 1853
-
-También puedes probar nuestro bot en este grupo:
-> https://chat.whatsapp.com/L1rd6UlRIaXC82vFLlMEop`.trim()
-await conn.sendFile(m.chat, gataImg, './Menu2.jpg', menu, fkontak, false, { contextInfo: {mentionedJid, externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' MichiBot ', previewType: 0, thumbnail: imagen4, sourceUrl: redesMenu }}})
+https://chat.whatsapp.com/GYVCOqyxXjhLqoeLyXr5xX`.trim()
+await conn.sendFile(m.chat, gataImg, './Menu3.jpg', menu, fkontak, false, { contextInfo: {mentionedJid, externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' MichiBot ', previewType: 0, thumbnail: imagen4, sourceUrl: redesMenu }}})
 //conn.sendFile(m.chat, gataVidMenu.getRandom(), 'gata.mp4', menu, fkontak)
 } catch (e) {
 await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
